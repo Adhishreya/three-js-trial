b54fa05f47773e9308a96e42fd040d148ab3415f
Adhishreya P <42477290+Adhishreya@users.noreply.github.com>
2021-05-06 19:01:59 +0530

    audio trial failed

--- src/script.js
+++ src/script.js
@@ -31,6 +31,22 @@ var texture=new THREE.TextureLoader().load('./galaxy.jpg');
 var material = new THREE.MeshBasicMaterial({ map:texture }); 
 
 
+//trying to add audio
+// create an AudioListener and add it to the camera
+const listener = new THREE.AudioListener();
+camera.add( listener );
+
+// create a global audio source
+const sound = new THREE.Audio( listener );
+
+// load a sound and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load( './sample.ogg', function( buffer ) {
+	sound.setBuffer( buffer );
+	sound.setLoop( true );
+	sound.setVolume( 0.5 );
+	sound.play();
+});
 //finally adding the scene elements to the DOM
 
 mesh = new THREE.Mesh(geometry, material);
